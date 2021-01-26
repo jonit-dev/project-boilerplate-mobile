88eae08532a9e4a37937892ecdea7c253e93d8aa
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-01-25 23:02:04 -0800

    comment test

--- src/pages/Main/Feed/Feed.screen.tsx
+++ src/pages/Main/Feed/Feed.screen.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { SideMenuPage } from "../../../components/SideMenu/SideMenuPage";
 
 export const FeedScreen: React.FC = () => {
+  // this is a comment
   return (
     <SideMenuPage>
       <IonCard>
